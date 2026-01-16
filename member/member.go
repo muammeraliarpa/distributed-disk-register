fb8d43a9b9d1371058453fdf973fbddd30b8ef62
Muammer Arpa <muammeraliarpa@gmail.com>
2026-01-16 12:46:33 +0300

    Unbuffered yazma kısmı eklendi.

--- member/member.go
+++ member/member.go
@@ -130,18 +130,19 @@ func (n *Node) ReplicateMessage(ctx context.Context, req *pb.ReplicateRequest) (
 
 	id := req.GetMessageId()
 	content := req.GetMessageContent()
-
-	// Dosyaya yazma işlemleri
 	filePath := fmt.Sprintf("%s/%s.txt", n.diskPath, id)
-	file, _ := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	writer := bufio.NewWriter(file)
+
+	fileUnbuf, _ := os.OpenFile(filePath+"_direct", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	fileUnbuf.Write([]byte(content))
+	fileUnbuf.Close()
+
+	fileBuf, _ := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	writer := bufio.NewWriter(fileBuf)
 	writer.WriteString(content)
 	writer.Flush()
-	file.Close()
+	fileBuf.Close()
 
 	n.storage[id] = content
-
-
 	return &pb.ReplicateResponse{Success: true}, nil
 }
 
